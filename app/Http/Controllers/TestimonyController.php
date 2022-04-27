16735c0207081e4cb64b1f32e1399f9b23056bdd
Stephane ASSOCLE <mensste30@gmail.com>
2022-04-27 20:11:41 +0100

    migration

---
+++ app/Http/Controllers/TestimonyController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestimonyController extends Controller
+{
+    public function store()
+    {
+        
+    }
+}
---
+++ database/migrations/2022_04_27_185400_create_countries_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCountriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->id();
+            $table->string('code',4)->unique();
+            $table->string('libelle', 45)->unique();
+            $table->engine = 'InnoDB';
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('countries');
+    }
+}
