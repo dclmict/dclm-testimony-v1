2bcde14106048a884cb21f2c926c5310a8ab1776
jkpeyi <jkpeyi@gmail.com>
2022-04-27 20:42:42 +0100

    first

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,30 @@
 
 
 @section('content')
-    <h1>
-        Testimony For
-        Ogun State Crusade
-    </h1>
+    <div class="row justify-content-center mt-3">
+        <div class="col-md-auto">
+            <a href="#">Contact us</a>
+        </div>
+        <div class="col-md-auto">
+            <a href="#">Go live</a>
+        </div>
+        <div class="col-md-auto">
+            <a href="#">Testify</a>
+        </div>
+    </div>
+    <div class="ml-5" style="margin-top:15%">
+        {{-- flex menu --}}
+
+
+        <h1 style="font-size: 72px">
+            Testimony For
+            Ogun State Crusade
+        </h1>
+        <h2 style="font-size: 36px">
+            Testify to the goodness of God
+        </h2>
+        <button class="btn main-button" style="margin-top:44px">
+            Share My Testimony
+        </button>
+    </div>
 @endsection
