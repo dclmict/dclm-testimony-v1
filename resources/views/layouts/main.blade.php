6caa3afbe8e4ddcc449286e48474f07f89c69be9
jkpeyi <jkpeyi@gmail.com>
2022-04-27 19:33:45 +0100

    first commit

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- imoort app.css --}}
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>Ogun state crusade testimony</title>
+    <style>
+        .main-side {
+            background: linear-gradient(178.89deg, #226091 -9.6%, #507DA0 0.42%, rgba(196, 196, 196, 0) 46.37%);
+            height: 100vh;
+        }
+
+        /* align center */
+        .icon {
+
+            margin-top: 10%;
+        }
+
+    </style>
+</head>
+
+<body class="container-fluid">
+    <div class="row">
+        <div class="col-md-6 main-side">
+
+                <img class="icon" width="500" src="{{ asset('images/icon.png') }}" alt="">
+                
+        </div>
+
+
+    </div>
+</body>
+
+</html>
