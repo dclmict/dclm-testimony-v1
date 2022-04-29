32e9b7bed9765b8ea035394345172dc0c83670f2
jkpeyi <jkpeyi@gmail.com>
2022-04-29 09:27:20 +0100

    social media links added

--- resources/views/thanks.blade.php
+++ resources/views/thanks.blade.php
@@ -5,15 +5,21 @@
         <img width="300" class="img-fluid" src="{{ asset('images/logo.png') }}" alt="">
         <span class="text-center" style="font-size: 36px;">
             THANKS <br>
-           FOR YOUR TESTIMONY
+            FOR YOUR TESTIMONY
         </span>
 
         <div class="d-flex flex-column align-items-center gap-3" style="margin-top: 10vh">
             Connect with us
             <div class="d-flex gap-4">
-                <img width="40" src="{{asset('images/yt.png')}}" alt="">
-                <img width="40" src="{{asset('images/fb.png')}}" alt="">
-                <img width="40" src="{{asset('images/tw.png')}}" alt="">
+                <a href="https://www.youtube.com/c/DCLMHQ" target="_blank"><img width="40"
+                        src="{{ asset('images/yt.png') }}" alt=""></a>
+
+                <a href="https://www.facebook.com/dclmhq" target="_blank"><img width="40"
+                        src="{{ asset('images/fb.png') }}" alt=""></a>
+                <a href="https://twitter.com/dclmhq" target="_blank"> <img width="40" src="{{ asset('images/tw.png') }}"
+                        alt=""></a>
+
+
             </div>
         </div>
     </div>
