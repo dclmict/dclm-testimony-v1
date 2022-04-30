e354918523b8addb45ede514c99f10119b8e1b12
jkpeyi <jkpeyi@gmail.com>
2022-04-30 09:53:16 +0100

    loading spinner on form submission

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -17,9 +17,10 @@
         <h4 class="text-center my-5">
             Share your testimony
         </h4>
-        <form action="{{ route('testimony.store') }}" method="POST" enctype="multipart/form-data">
+        <form action="{{ route('testimony.store') }}" method="POST" enctype="multipart/form-data" x-data="app()">
             @csrf
             <div class="my-3">
+
                 <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Fullname"
                     name="full_name" value="{{ old('full_name') }}">
                 <x-error name="full_name" />
@@ -64,22 +65,38 @@
             <div class="row my-5">
                 <div class="col-md-6">
                     {{-- <button type="button" class="btn col-12 btn-outline-primary mt-2"> Upload your testimony </button> --}}
-                    <input type="file"  name="file_dir" class="btn col-12 btn-outline-primary mt-2" id="file_dir"
+                    <input type="file" name="file_dir" class="btn col-12 btn-outline-primary mt-2" id="file_dir"
                         value="{{ old('file_dir') }}">
                     <x-error name="file_dir" />
                 </div>
                 <div class="col-md-6">
-                    <button class="btn col-12 btn-primary mt-2" type="submit"> Submit </button>
+                    <button x-on:click="submit()" class="btn col-12 btn-primary mt-2 d-flex justify-content-center align-items-center" type="submit"> <span
+                            x-text="button_text" >Submit </span>
+                        <div x-show="loading" class="spinner-border text-white" role="status" id="spinner">
+
+                        </div>
+                    </button>
                 </div>
-            </div>
+
+                {{-- spiner bootstrap --}}
+
+                {{-- <div class="progress" >
+                    <div class="progress-bar" x-ref="progress_bar" :style="`width:${progressValue}+'%'`" role="progressbar"  aria-valuenow="0" aria-valuemin="0"
+                        aria-valuemax="100" x-text="progressValue+'%'">25%</div>
+                </div> --}}
         </form>
     </div>
 @endsection
 
 @push('scripts')
+    <script src="{{ asset('js/alpine.min.js') }}">
+
+    </script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"
         integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="{{ asset('js/jquery.form.js') }}">
 
+    </script>
 
 
     <script src="{{ asset('js/select2.js') }}"></script>
@@ -92,5 +109,35 @@
         $(document).on('select2:open', () => {
             document.querySelector('.select2-search__field').focus();
         });
+    </script>
+
+    <script>
+        window.app = function() {
+            return {
+                loading: false,
+                button_text: 'Submit',
+                progressValue: 0,
+
+                submit() {
+                    this.loading = true;
+                    this.button_text = 'Submitting...';
+
+                   /*  setInterval(() => {
+
+                        this.progressValue += Math.floor(Math.random() * 10);
+
+                        // $refs.progress_bar.style.width = this.progressValue + '%';
+                        //in nextick update $refs.progress_bar.style value
+
+                        if (this.progressValue >= 100) {
+                            this.progressValue = 99;
+                        }
+                    }, 1000);
+                    */
+
+                },
+
+            }
+        }
     </script>
 @endpush
