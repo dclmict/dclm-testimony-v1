7ddaeece6b3ed5240c7f040689dfd851492c944a
jkpeyi <jkpeyi@gmail.com>
2022-05-19 01:37:56 +0100

    Fix the mobile front end responsvity issue

--- app/Models/Testifier.php
+++ app/Models/Testifier.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Testimony;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Testifier extends Model
 {
     use HasFactory;
+
+
+    public function testimonies(){
+        return $this->hasMany(Testimony::class);
+    }
 }
