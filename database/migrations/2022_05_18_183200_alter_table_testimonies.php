f90304c677937e197e6f40e9c933712a82e0a263
jkpeyi <jkpeyi@gmail.com>
2022-05-22 20:55:17 +0100

    try catch migrations testmonies

--- database/migrations/2022_05_18_183200_alter_table_testimonies.php
+++ database/migrations/2022_05_18_183200_alter_table_testimonies.php
@@ -14,12 +14,15 @@ class AlterTableTestimonies extends Migration
     public function up()
     {
         //
-        Schema::table("testimonies", function(Blueprint $table){
+        Schema::table("testimonies", function (Blueprint $table) {
             $table->foreignId("testifier_id")->nullable();
 
-            $table->dropForeign("testimonies_country_id_foreign");
-            $table->dropColumn(["full_name", "country_id", "city" , "email" , "phone"]);
-
+            try {
+                $table->dropForeign("testimonies_country_id_foreign");
+                $table->dropColumn(["full_name", "country_id", "city", "email", "phone"]);
+            } catch (\Throwable $th) {
+                //throw $th;
+            }
         });
     }
 
@@ -31,7 +34,7 @@ class AlterTableTestimonies extends Migration
     public function down()
     {
         //
-        Schema::table("testimonies", function(Blueprint $table){
+        Schema::table("testimonies", function (Blueprint $table) {
             $table->dropForeign("testimonies_testifier_id_foreign");
             $table->dropColumn("testifier_id");
         });
