b441a506a9fc90ea72c052b3967f6131dce41998
mimigraphy20 <jollydosky@gmail.com>
2022-05-23 14:48:12 +0100

    fixed error for testimonyFormcontroller conflictting names

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\TestimonyFormController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CrusadeTourController;
-use App\Http\Controllers\TestimonyFormController;
 
 /*
 |--------------------------------------------------------------------------
