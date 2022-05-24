4c864cc0575919fd6338be67f5e49f79d8944c13
jkpeyi <jkpeyi@gmail.com>
2022-05-24 23:35:06 +0100

    cc

--- app/Models/Testimony.php
+++ app/Models/Testimony.php
@@ -75,7 +75,10 @@ class Testimony extends Model
     {
 
         try {
-            return $this->file_dir ? Storage::disk('s3')->url("dclm-testimony/" . $this->crusadeTour->slug . "/" . $this->file_dir) : null;
+            //get presigned url
+            //$url = Storage::disk('s3')->temporaryUrl("dclm-testimony/" . $this->crusade_tour->slug . "/" . $this->file_dir, now()->addMinutes(5));
+            return $this->file_dir ? Storage::disk('s3')
+                ->temporaryUrl("dclm-testimony/" . $this->crusadeTour->slug . "/" . $this->file_dir, now()->addYears(10)) : null;
         } catch (\Throwable $th) {
 
             Log::error($th->getMessage());
