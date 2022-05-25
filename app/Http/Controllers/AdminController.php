1682b1b180c953328cec081c887e83f1359f652d
mimigraphy20 <jollydosky@gmail.com>
2022-05-25 21:12:48 +0100

    updated the repo with admin name

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -12,7 +12,7 @@ class AdminController extends Controller
     public function index()
     {
         $active = CrusadeTour::whereIsActive(true)->first();
-        $user = 'edu';
+        $user = 'Admin';
 
         return view('admin.index', compact('active',))->with('user', $user);
     }
