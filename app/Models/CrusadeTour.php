2b0913cbce379cf5091eab365f743619f772e95b
jkpeyi <jkpeyi@gmail.com>
2022-09-22 15:26:26 +0100

    bug fixed

--- app/Models/CrusadeTour.php
+++ app/Models/CrusadeTour.php
@@ -19,7 +19,7 @@ class CrusadeTour extends Model
 
 
         if ($file) {
-            $crusadeTour->banner_path=  $crusadeTour->saveFile($file);
+            $crusadeTour->banner_path=  $crusadeTour->storeFile($file);
         }
 
         $crusadeTour->save();
