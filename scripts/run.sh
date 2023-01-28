c47166ea327a5050b900afc7815993249674e591
Samuel John <opeoniye@gmail.com>
2023-01-28 21:17:08 +0100

    automating dhparam generation for nginx ssl setup

---
+++ scripts/run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#
+# ____          __  __  _____ 
+# |  _ \   /\   |  \/  |/ ____|     https://github.com/opeoniye
+# | |_) | /  \  | \  / | (___       https://opeoniye.vercel.app/
+# |  _ < / /\ \ | |\/| |\___ \      credit: http://patorjk.com/software/taag/
+# | |_) / ____ \| |  | |____) |
+# |____/_/    \_\_|  |_|_____/ 
+#                             
+#
+# Based on https://gist.github.com/2206527
+
+
+openssl dhparam -out /etc/ssl/certs/dhparams.pem 4096
